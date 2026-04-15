1a1c07389c400d8a941ffa602a41532666cf21fa
nataliatsi <nataliandsg32@gmail.com>
2026-04-15 15:48:45 -0300

    config: adicionar profile de teste com application-test.yml

--- portal/src/test/java/br/com/progirls/api/portal/PortalApplicationTests.java
+++ portal/src/test/java/br/com/progirls/api/portal/PortalApplicationTests.java
@@ -2,7 +2,9 @@ package br.com.progirls.api.portal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class PortalApplicationTests {
 
